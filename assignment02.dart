69c440b8c93b3f898ac5e8de59182aa5a7bf8d75
Shaharyar <shaharyar4t4@gmail.com>
2024-08-28 23:33:27 +0500

    assignment 02 updated

--- assignment02.dart
+++ assignment02.dart
@@ -2,7 +2,7 @@ import 'dart:io';
 
 void main() {
   //Q.1 Write a Dart program to check if a given number is positive, negative, or zero.
-  /*print("Enter the number:");
+  print("Enter the number:");
   String? num = stdin.readLineSync();
   int Num = int.parse(num!);
 
@@ -12,8 +12,9 @@ void main() {
     print('This is a Negative Value: $Num');
   } else {
     print('The give value is ZERO');
-  }*/
+  }
 
+  //Q.2: Write a Dart program to check if a given number is even or odd.
   print("Enter the number:");
   String? num1 = stdin.readLineSync();
   int Num1 = int.parse(num1!);
@@ -21,5 +22,24 @@ void main() {
     print('This is Even Number: $Num1');
   } else {
     print('This is odd Number: $Num1');
+  }
+
+  //Q.4: Develop a Dart program to find the largest of three numbers.
+  print("Enter the Value of A:");
+  String? a = stdin.readLineSync();
+  int A = int.parse(a!);
+  print("Enter the Value of B:");
+  String? b = stdin.readLineSync();
+  int B = int.parse(b!);
+  print("Enter the Value of C:");
+  String? c = stdin.readLineSync();
+  int C = int.parse(c!);
+
+  if (A >= B && A >= C) {
+    print('The largest  Value of A: $A ');
+  } else if (B >= C && B >= A) {
+    print('The largest  Value of B: $B');
+  } else {
+    print('The largest  Value of C: $C');
   }
 }
