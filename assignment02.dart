dd464b4d449eea7595c348eeeaac7dfca61a5db5
Shaharyar <shaharyar4t4@gmail.com>
2024-08-29 02:11:58 +0500

    assignment 02 updated

--- assignment02.dart
+++ assignment02.dart
@@ -1,7 +1,7 @@
 import 'dart:io';
 
 void main() {
-  //Q.1 Write a Dart program to check if a given number is positive, negative, or zero.
+/*  //Q.1 Write a Dart program to check if a given number is positive, negative, or zero.
   print("Enter the number:");
   String? num = stdin.readLineSync();
   int Num = int.parse(num!);
@@ -22,9 +22,20 @@ void main() {
     print('This is Even Number: $Num1');
   } else {
     print('This is odd Number: $Num1');
+  }*/
+
+  //Q.5: Write a Dart program to determine if a given year is a leap year or not, considering leap years occur every 4 years except for years divisible by 100 unless they are also divisible by 400.
+  print('Enter your year:');
+  String? year = stdin.readLineSync();
+  int Year = int.parse(year!);
+
+  if ((Year % 4 == 0 && Year % 100 != 0) || (Year % 400 == 0)) {
+    print('This is leap year: $year');
+  } else {
+    print(' This is not a leap year: $year');
   }
 
-  //Q.4: Develop a Dart program to find the largest of three numbers.
+  /*//Q.4: Develop a Dart program to find the largest of three numbers.
   print("Enter the Value of A:");
   String? a = stdin.readLineSync();
   int A = int.parse(a!);
@@ -42,4 +53,16 @@ void main() {
   } else {
     print('The largest  Value of C: $C');
   }
+  
+
+  //Q.6:Write a Dart program to check if a given year is a century year (ends with 00)?
+  print('Enter your year:');
+  String? year = stdin.readLineSync();
+  int Year = int.parse(year!);
+
+  if (Year % 100 == 0) {
+    print('The given year in century $Year');
+  } else {
+    print('The give is not in  century $Year');
+  }*/
 }
