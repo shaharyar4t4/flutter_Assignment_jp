052b3d2e3d6786dc1e847bd3b9546d6e6b414a51
Shaharyar <shaharyar4t4@gmail.com>
2024-09-03 22:36:13 +0500

    updated

---
+++ main.dart
@@ -0,0 +1,15 @@
+void main() {
+  // List liname = ["hassan", "Muneeb", "Sameer", "Fahad", "Nasir", "Maaz"];
+  // print(liname);
+
+  List lisday = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday"
+  ];
+  print(lisday[6]);
+}
