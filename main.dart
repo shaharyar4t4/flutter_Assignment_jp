e664b188cafd3473cc55f4501e82b64ce6e2f607
Shaharyar <shaharyar4t4@gmail.com>
2024-09-26 01:27:27 +0500

    updated

---
+++ main.dart
@@ -0,0 +1,20 @@
+void main() {
+  // List name = ["Hassan", "Fahad", "Muneeb", "Sameer", "Ahmed"];
+  // print(name);
+
+  List<String> daysName = [];
+  daysName.add("Monday");
+  //print(daysName);
+  daysName.add("Tuesday");
+  //print(daysName);
+  daysName.add("Wednesday");
+  //print(daysName);
+  daysName.add("Thusday");
+  //print(daysName);
+  daysName.add("Friday");
+  //print(daysName);
+  daysName.add("Saturday");
+  //print(daysName);
+  daysName.add("Sunday");
+  print(daysName);
+}
