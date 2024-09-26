809ffdca7df2a6a20e70215ab9c738a4cd1836d2
Shaharyar <shaharyar4t4@gmail.com>
2024-09-26 22:20:08 +0500

    updated

--- main.dart
+++ main.dart
@@ -44,20 +44,57 @@ void main() {
   // daysNames.remove("Sunday");
   //print(daysNames);
 
-  List<int> Nulis = [3, 9, 1, 6, 4, 2, 8, 5, 7];
+  // List<int> Nulis = [3, 9, 1, 6, 4, 2, 8, 5, 7];
 
-  int max = Nulis[0];
-  int min = Nulis[0];
+  // int max = Nulis[0];
+  // int min = Nulis[0];
 
-  for (int i = 1; i < Nulis.length; i++) {
-    if (Nulis[i] > max) {
-      max = Nulis[i];
-    } else if (Nulis[0] < min) {
-      min = Nulis[i];
-    } else {
-      break;
-    }
-  }
-  print("Your largest value  is $max");
-  print("Your largest value  is $min");
+  // for (int i = 1; i < Nulis.length; i++) {
+  //   if (Nulis[i] > max) {
+  //     max = Nulis[i];
+  //   } else if (Nulis[0] < min) {
+  //     min = Nulis[i];
+  //   } else {
+  //     break;
+  //   }
+  // }
+  // print("Your largest value  is $max");
+  // print("Your largest value  is $min");
+
+  // Map student = {
+  //   "Taha": "039392899",
+  //   "hassan": "04533223",
+  //   "Shan": "0289393",
+  //   "Ahmed": "04533223",
+  // };
+
+  // print("the key value length is 4: ");
+  // student.forEach((key, value) {
+  //   if (key.length == 4) {
+  //     print(key);
+  //   }
+  // });
+
+  // Map world = {
+  //   "countries": {
+  //     "Pakistan": {
+  //       "Captial": "Islamabad",
+  //       "Currency": "Rupee",
+  //       "Language": "Urdu",
+  //     },
+  //     "France": {
+  //       "Captial": "Paris",
+  //       "Currency": "Euro",
+  //       "Language": "French",
+  //     },
+  //     "italy": {
+  //       "Captial": "Rome",
+  //       "Currency": "Euro",
+  //       "Language": "italain",
+  //     },
+  //   }
+  // };
+  // var Countries = world["countries"]["Pakistan"];
+  // print("The  Captial of Pakistan is: ${Countries["Captial"]}");
+  // print("The Currency  of Pakistan is: ${Countries["Currency"]}");
 }
